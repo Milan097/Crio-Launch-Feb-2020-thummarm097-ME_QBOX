0e7f2b42e892d0350a42f84462de01047cdaf5b0
Milan097 <thummarm097@gmail.com>
2020-01-31 19:09:01 +0000

    ME_QBOX_MODULE_LINUX_COMMANDS

--- linux_commands/print_home_directory.sh
+++ linux_commands/print_home_directory.sh
@@ -6,4 +6,4 @@
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
     # 3) Execute the script before submission and confirm the output
 
-
+    eval echo ~$USER
